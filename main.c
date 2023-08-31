9ff7d78d7b07823d0a851274f734f40078b730e3
Nkemjika1divine <cndivine@gmail.com>
2023-08-31 16:21:04 -0700

    RSA Factoring Challenge

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	char line[50];
 	int number, i, answer;
 
-	if (argc == 0)
+	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: factor <file>\n");
 		exit(EXIT_FAILURE);
