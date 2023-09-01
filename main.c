f37242e346f8d22e29eebe17351815473de61d3a
Nkemjika1divine <cndivine@gmail.com>
2023-09-01 01:02:42 -0700

    RSA Factoring Challenge

--- main.c
+++ main.c
@@ -3,6 +3,53 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+
+
 /**
  * main - runs the program
  * @argc: argument count
@@ -13,8 +60,8 @@
 int main(int argc, char *argv[])
 {
 	FILE *fd;
-	char line[50];
-	int number, i, answer;
+	char line[50], *end;
+	long answer, number, i;
 
 	if (argc != 2)
 	{
@@ -32,7 +79,7 @@ int main(int argc, char *argv[])
 	while (fgets(line, sizeof(line), fd) != NULL)
 	{
 		line[strcspn(line, "\n")] = '\0';
-		number = atoi(line);
+		number = strtol(line, &end, 10);
 		if (number <= 0)
 			fprintf(stderr, "String cant be converted\n");
 		else
@@ -42,7 +89,7 @@ int main(int argc, char *argv[])
 				if ((number % i) == 0)
 				{
 					answer = number / i;
-					printf("%d=%d*%d\n", number, answer, i);
+					printf("%ld=%ld*%ld\n", number, answer, i);
 					break;
 				}
 			}
